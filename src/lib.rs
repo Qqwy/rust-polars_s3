b5779088deaaec3a23ef65c4a9db36704206c6fd
qqwy <qqwy@gmx.com>
2023-07-17 20:12:24 +0200

    By waiting on the task, it works!

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ mod tests {
         use polars::df;
         use polars::prelude::NamedFrom;
         use polars::prelude::{CsvWriter, SerWriter};
+        use std::io::Write;
 
         let mut df = df!(
             "foo" => &[1, 2, 3],
@@ -21,8 +22,16 @@ mod tests {
         let path: object_store::path::Path = "object_store_example.csv".into();
         let (id, mut async_s3_writer) = object_store.put_multipart(&path).await.expect("Could not create location to write to");
         let mut sync_s3_uploader = tokio_util::io::SyncIoBridge::new(async_s3_writer);
+        // let mut sync_s3_uploader = tokio_util::io::SyncIoBridge::new(tokio_util::io::InspectWriter::new(async_s3_writer, |data|{
+        //     dbg!(data);
+        // }));
+        let join_handle = tokio::task::spawn_blocking(move || {
+            CsvWriter::new(&mut sync_s3_uploader).finish(&mut df).expect("Could not write dataframe as CSV to remote location");
+            sync_s3_uploader.flush().unwrap();
+            sync_s3_uploader.shutdown().unwrap();
+        });
+        let res = join_handle.await;
 
-        CsvWriter::new(&mut sync_s3_uploader).finish(&mut df).unwrap();
     }
 
     #[test]
