22dbfce5fc20c54212aa8b2ca8aa1c2042a69801
qqwy <qqwy@gmx.com>
2023-07-17 19:42:58 +0200

    Basic test from example page works

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn csv_to_file() {
+        use polars::df;
+        use polars::prelude::NamedFrom;
+        use polars::prelude::{CsvWriter, SerWriter};
+
+        let mut df = df!(
+            "foo" => &[1, 2, 3],
+            "bar" => &[None, Some("bak"), Some("baz")],
+        )
+            .unwrap();
+
+        let mut file = std::fs::File::create("path.csv").unwrap();
+        CsvWriter::new(&mut file).finish(&mut df).unwrap();
     }
 }
