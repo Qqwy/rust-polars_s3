f63f1a0a10e7431f99a35b5ba2d03695a992ae6f
qqwy <qqwy@gmx.com>
2023-07-17 19:53:12 +0200

    Basic test for the object store. Does not work yet; Tokio complains

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,29 @@
 #[cfg(test)]
 mod tests {
+    use object_store::ObjectStore;
+
     use super::*;
+
+    #[tokio::test]
+    async fn csv_to_local_objectstore_file() {
+        use polars::df;
+        use polars::prelude::NamedFrom;
+        use polars::prelude::{CsvWriter, SerWriter};
+
+        let mut df = df!(
+            "foo" => &[1, 2, 3],
+            "bar" => &[None, Some("bak"), Some("baz")],
+        )
+            .unwrap();
+
+        let object_store = object_store::local::LocalFileSystem::new_with_prefix("/tmp/").expect("Could not initialize connection");
+
+        let path: object_store::path::Path = "object_store_example.csv".into();
+        let (id, mut async_s3_writer) = object_store.put_multipart(&path).await.expect("Could not create location to write to");
+        let mut sync_s3_uploader = tokio_util::io::SyncIoBridge::new(async_s3_writer);
+
+        CsvWriter::new(&mut sync_s3_uploader).finish(&mut df).unwrap();
+    }
 
     #[test]
     fn csv_to_file() {
@@ -14,7 +37,7 @@ mod tests {
         )
             .unwrap();
 
-        let mut file = std::fs::File::create("path.csv").unwrap();
+        let mut file = std::fs::File::create("/tmp/example.csv").unwrap();
         CsvWriter::new(&mut file).finish(&mut df).unwrap();
     }
 }
